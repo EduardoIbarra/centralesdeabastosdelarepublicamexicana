9c70ef74473daef7d99cfbc6532f90e5d75666e0
Miguel Hernandez <miguelhdzch@gmail.com>
2018-02-19 00:36:12 -0600

    Adding splash and icon, adding basic pages and menu style

---
+++ mobile/src/pages/infox/infox.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { InfoxPage } from './infox';
+
+@NgModule({
+  declarations: [
+    InfoxPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(InfoxPage),
+  ],
+})
+export class InfoxPageModule {}
