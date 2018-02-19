bbd037ba60b1c9bc4069e082958285f9d266dc09
Miguel Hernandez <miguelhdzch@gmail.com>
2018-02-19 03:27:39 -0600

    Adding about page informations, adding reusable accordion component

--- mobile/src/pages/about/about.module.ts
+++ mobile/src/pages/about/about.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { AboutPage } from './about';
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {AboutPage} from './about';
+import {AccordionListComponentModule} from "../../components/accordion-list/accordion-list.module";
 
 @NgModule({
-  declarations: [
-    AboutPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(AboutPage),
-  ],
+    declarations: [
+        AboutPage,
+    ],
+    imports: [
+        IonicPageModule.forChild(AboutPage),
+        AccordionListComponentModule
+    ],
+    exports:[
+        AboutPage
+    ]
 })
-export class AboutPageModule {}
+export class AboutPageModule {
+}
