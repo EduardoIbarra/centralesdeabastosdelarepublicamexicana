e604c9f638e84003b55f04404ceab8d17434b4f3
Miguel Hernandez <miguelhdzch@gmail.com>
2018-02-22 13:15:23 -0600

    Latest changes

--- mobile/src/pages/home/home.ts
+++ mobile/src/pages/home/home.ts
@@ -10,10 +10,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class HomePage {
 
     imgaes = [
-        {
-            imageUrl: 'assets/img/carousel/1.jpg',
-            quote: 'Mas de 115 años impulsando el Comercio y Turismo en Querétaro.',
-        },
+        // {
+        //     imageUrl: 'assets/img/carousel/1.jpg',
+        //     quote: 'Mas de 115 años impulsando el Comercio y Turismo en Querétaro.',
+        // },
         {
             imageUrl: 'assets/img/carousel/2.jpg',
             quote: 'Paga tu Predial en Febrero con un descuento del 8%',
@@ -26,5 +26,9 @@ export class HomePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
   }
+
+    goToPage(page){
+        this.navCtrl.push(page);
+    }
 
 }
